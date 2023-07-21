2d5312578e6cf273ce0b4567e2d6cc8571f6da48
x2d7751347m <x2d7751347m@gmail.com>
2023-07-21 12:14:00 +0900

    restore tests

--- src/test/kotlin/com/x2d7751347m/ApplicationTest.kt
+++ src/test/kotlin/com/x2d7751347m/ApplicationTest.kt
@@ -38,88 +38,88 @@ class ApplicationTest {
     }
 
 //    @Test
-//    fun testWebSocket() = testApplication {
-//        application {
-////                testModule()
-//        }
-//
-//        val handler = CoroutineExceptionHandler { _, exception ->
-//            println("CoroutineExceptionHandler got $exception")
-//        }
-//        val client = HttpClient(CIO) {
-//            engine {
-//                endpoint.maxConnectionsPerRoute = 2000
-//                endpoint.socketTimeout = 15000
-//                endpoint.connectAttempts = 3
-//                endpoint.connectTimeout = 15000
-//            }
-//            install(WebSockets) {
-//                pingInterval = 15_000
-//            }
-//        }
-//
-//        fun currentMoment(): Instant = Clock.System.now()
-//        // Fps of Client
-//        val framePerSecond = 60.0
-//        val sendingInterval = (1000 / framePerSecond)
-//        coroutineScope {
-//            // Number of Clients
-//            repeat(10) {
-//                launch(Dispatchers.Default) {
-////                    var lastSentTime = mutableMapOf(Pair(it, currentMom  nt().toEpochMilliseconds()))
-//                    var lastSentTime = currentMoment()
-//                    client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
-//                        timeoutMillis = 15000
-//                        try {
-//                            runBlocking {
-//                                launch(Dispatchers.IO) {
-//                                    while (true) {
-//                                        when (incoming.receive()) {
-//                                            is Frame.Text -> {
-//                                                val othersMessage = incoming.receive() as? Frame.Text
-////                                            println("[$it]: ${othersMessage?.readText()}")
-//                                                if (othersMessage?.readText()?.lowercase()
-//                                                        ?.startsWith("did you still alive") == true
-//                                                ) {
-//                                                    send("[$it]: Yeah")
-//                                                    println("[$it]: Yeah")
-//                                                }
-//                                            }
-//
-//                                            else -> {}
-//                                        }
-//                                    }
-//                                }
-//                                launch(Dispatchers.IO) {
-//                                    while (true) {
-//                                        if (currentMoment().minus(lastSentTime).inWholeMilliseconds > sendingInterval) {
-//                                            send("[$it]: $lastSentTime")
-//                                            lastSentTime = currentMoment()
-//                                        }
-//                                    }
-//                                }
-//
-//
-//                            }
-//                        } catch (e: Exception) {
-//                            println(e.localizedMessage)
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    // for test raw socket
+    fun testWebSocket() = testApplication {
+        application {
+//                testModule()
+        }
+
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("CoroutineExceptionHandler got $exception")
+        }
+        val client = HttpClient(CIO) {
+            engine {
+                endpoint.maxConnectionsPerRoute = 2000
+                endpoint.socketTimeout = 15000
+                endpoint.connectAttempts = 3
+                endpoint.connectTimeout = 15000
+            }
+            install(WebSockets) {
+                pingInterval = 15_000
+            }
+        }
+
+        fun currentMoment(): Instant = Clock.System.now()
+        // Fps of Client
+        val framePerSecond = 60.0
+        val sendingInterval = (1000 / framePerSecond)
+        coroutineScope {
+            // Number of Clients
+            repeat(10) {
+                launch(Dispatchers.Default) {
+//                    var lastSentTime = mutableMapOf(Pair(it, currentMom  nt().toEpochMilliseconds()))
+                    var lastSentTime = currentMoment()
+                    client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+                        timeoutMillis = 15000
+                        try {
+                            runBlocking {
+                                launch(Dispatchers.IO) {
+                                    while (true) {
+                                        when (incoming.receive()) {
+                                            is Frame.Text -> {
+                                                val othersMessage = incoming.receive() as? Frame.Text
+//                                            println("[$it]: ${othersMessage?.readText()}")
+                                                if (othersMessage?.readText()?.lowercase()
+                                                        ?.startsWith("did you still alive") == true
+                                                ) {
+                                                    send("[$it]: Yeah")
+                                                    println("[$it]: Yeah")
+                                                }
+                                            }
+
+                                            else -> {}
+                                        }
+                                    }
+                                }
+                                launch(Dispatchers.IO) {
+                                    while (true) {
+                                        if (currentMoment().minus(lastSentTime).inWholeMilliseconds > sendingInterval) {
+                                            send("[$it]: $lastSentTime")
+                                            lastSentTime = currentMoment()
+                                        }
+                                    }
+                                }
+
+
+                            }
+                        } catch (e: Exception) {
+                            println(e.localizedMessage)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // for test raw socket
 //    @Test
-//    fun socketClient() {
-//        val client = Socket("127.0.0.1", 9002)
-//        val output = PrintWriter(client.getOutputStream(), true)
-//        val input = BufferedReader(InputStreamReader(client.inputStream))
-//
-//        println("Client sending [Hello]")
-//        output.println("Hello")
-//        println("Client receiving [${input.readLine()}]")
-//        client.close()
-//    }
+    fun socketClient() {
+        val client = Socket("127.0.0.1", 9002)
+        val output = PrintWriter(client.getOutputStream(), true)
+        val input = BufferedReader(InputStreamReader(client.inputStream))
+
+        println("Client sending [Hello]")
+        output.println("Hello")
+        println("Client receiving [${input.readLine()}]")
+        client.close()
+    }
 }
